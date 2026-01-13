cfc8d21aaff75067714379ee1f9f03b20aad6c1d
aleynaakyuz <aleynaakyuz1@gmail.com>
2026-01-13 13:51:22 -0500

    adding small bank

---
+++ small_bank/tau_q/gen_bank.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ OMP_NUM_THREADS=1 \
+ pycbc_brute_bank \
+ --verbose \
+ --output-file part-1.hdf \
+ --minimal-match 0.965 \
+ --tolerance .005 \
+ --full-resolution-buffer-length 140 \
+ --buffer-length 4 \
+ --sample-rate 2048 \
+ --tau0-threshold 0.5 \
+ --tau0-start 100.0 \
+ --tau0-end 190.0 \
+ --approximant teobresums \
+ --tau0-crawl 10 \
+ --asd-file ./o3asd.txt \
+ --seed 1 \
+ --low-frequency-cutoff 20.0 \
+ --tau0-cutoff-frequency 20.0 \
+ --input-config ./priors.ini \
+ --nprocesses 32
