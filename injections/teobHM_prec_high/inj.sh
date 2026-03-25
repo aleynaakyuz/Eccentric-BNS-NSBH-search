e6746ee118985bc814ff37d5ff975842e0998453
aleynaakyuz <aleynaakyuz1@gmail.com>
2026-03-24 22:06:59 -0400

    adding injections

---
+++ injections/teobHM_prec_high/inj.sh
@@ -0,0 +1,1 @@
+pycbc_create_injections --verbose --config-files config.ini --output-file teobHM_prec_high.hdf --gps-start-time 1368195220 --gps-end-time 1389456018 --time-step 100 --time-window 100 --seed 1 --force
